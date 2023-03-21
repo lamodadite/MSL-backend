200bdf6fdb6c62ea34a026672b0f0471c771820c
이종민 <102534186+lamodadite@users.noreply.github.com>
2023-03-22 00:42:03 +0900

    refactor : service 위에 @Transactional 제거 제거하고 필요한 메서드마다 각각 달아줌

--- MSL-back/src/main/java/Maswillaeng/MSLback/service/AwsS3Service.java
+++ MSL-back/src/main/java/Maswillaeng/MSLback/service/AwsS3Service.java
@@ -2,21 +2,16 @@ package Maswillaeng.MSLback.service;
 
 import Maswillaeng.MSLback.common.exception.S3FileUploadException;
 import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.tomcat.util.http.fileupload.FileUploadException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -25,7 +20,6 @@ import java.util.UUID;
 @Slf4j
 @RequiredArgsConstructor
 @Service
-@Transactional
 public class AwsS3Service {
 
     private final AmazonS3Client amazonS3Client;
@@ -33,30 +27,30 @@ public class AwsS3Service {
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
 
-
-    public  Map<String,String> AwsUploadImage(MultipartFile imageFile) {
+    @Transactional
+    public Map<String, String> AwsUploadImage(MultipartFile imageFile) {
         String uploadUrl = "";
         try {
             byte[] imageData = imageFile.getBytes();
             UUID uuid = UUID.randomUUID();
             String savedFileName = uuid.toString() + "_" + imageFile.getOriginalFilename();
             uploadUrl = s3uploadImage(savedFileName, imageFile);
-        }catch (Exception e){
+        } catch (Exception e) {
             throw new S3FileUploadException();
         }
-        Map<String,String> imagePath = new HashMap<>();
-        imagePath.put("img",uploadUrl);
+        Map<String, String> imagePath = new HashMap<>();
+        imagePath.put("img", uploadUrl);
 
         return imagePath;
     }
 
-    public String s3uploadImage(String savedFileName,MultipartFile imageFile) throws IOException {
+    private String s3uploadImage(String savedFileName, MultipartFile imageFile) throws IOException {
         ObjectMetadata metadata = new ObjectMetadata();
         metadata.setContentLength(imageFile.getSize());
 
-        amazonS3Client.putObject(new PutObjectRequest(bucket, savedFileName,imageFile.getInputStream(),metadata));
+        amazonS3Client.putObject(new PutObjectRequest(bucket, savedFileName, imageFile.getInputStream(), metadata));
 
-        return amazonS3Client.getUrl(bucket,savedFileName).toString();
+        return amazonS3Client.getUrl(bucket, savedFileName).toString();
     }
 
 }
