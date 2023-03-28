7448f396de97c1a956d968137e61054846eec1f9
이종민 <102534186+lamodadite@users.noreply.github.com>
2023-03-28 15:07:44 +0900

    test : test 일시적으로 주석처리

--- src/test/java/Maswillaeng/MSLback/MslBackApplicationTests.java
+++ src/test/java/Maswillaeng/MSLback/MslBackApplicationTests.java
@@ -1,13 +1,8 @@
-package Maswillaeng.MSLback;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class MslBackApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package Maswillaeng.MSLback;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class MslBackApplicationTests {
+//}
